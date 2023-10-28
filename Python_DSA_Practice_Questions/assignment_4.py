e46faaff397065173122d2a08d4c189f5c6bcf60
Kartik <iamkaranmishra551@gmail.com>
2023-10-28 06:26:00 +0530

    Solve all practice questions except question 12.

--- Python_DSA_Practice_Questions/assignment_4.py
+++ Python_DSA_Practice_Questions/assignment_4.py
@@ -1,4 +1,5 @@
 # ----------------- Doubly Linked List Questions -------------
+# Que:9 (Ok tested).
 class Iterator:
     def __init__(self, current):
         self.current = current
@@ -37,39 +38,107 @@ class DLL:
     def is_empty(self):
         return self.start is None
 
-    # Que:4
+    # Que:4 (Ok Tested.)
     def insert_at_start(self, data):
         node_object = Node(None, data, self.start)
         # if DLL is not empty then
         if not self.is_empty():
             self.start.prev = node_object
-        # connect the node with start.(If node is empty then do this)
-        self.start = node_object
+            # connect the node with start
+            self.start = node_object
+        else:
+            self.start = node_object
+            node_object.next = None
 
-    # Que:-5
+    # Que:-5   ( Ok Tested. )
     def insert_at_last(self, data):
+        # Get the reference of first node_object.
         temp = self.start
         if not self.is_empty():
             while temp.next is not None:
                 temp = temp.next
                 # we get last node reference in temp variable after
                 # ending the loop.
-        # temp None means DLL contains only one element.
+        # create a node_object.
         node_object = Node(temp, data, None)
+        # temp(self.start) None means DLL contains only one element.
         if temp is None:
             self.start = node_object
         else:
-            # node_object = Node(temp, data, None)
+            # join the node with second last node_object.
             temp.next = node_object
 
+    # Que:6 (Ok Tested)
+    def search(self, data):
+        if self.is_empty():
+            return None
+        temp = self.start
+        while temp is not None:
+            if temp.item == data:
+                return True
+            temp = temp.next
+        else:
+            return False
+
+    # Que:7 (Ok Tested)
+    def insert_after(self, search_data, insert_data):
+        if self.is_empty():
+            return None
+        temp = self.start
+        if self.search(search_data):
+            while temp is not None:
+                if temp.item == search_data:
+                    node_object = Node(temp, insert_data, temp.next)
+                    temp.next = node_object
+                temp = temp.next
+
+    # Que:8 (Ok Tested)
     def printObject(self):
+        # Get the reference of first node_object.
         temp = self.start
         while temp is not None:
             print(temp.item, end=' ')
             temp = temp.next
 
+    # Que:9
     def __iter__(self):
         return Iterator(self.start)
+
+    # Que:10 (Ok Tested.)
+    def delete_first(self):
+        # if list is empty then do nothing.
+        if self.is_empty():
+            pass
+        # if list contains only one node_object.
+        if self.start.next is None:
+            self.start = None
+        # If list contains at least two node objects.
+        else:
+            self.start = self.start.next
+            self.start.prev = None
+
+    # Que:11 (Ok Tested)
+    def delete_last(self):
+        # Get the reference of first Node.
+        if not self.is_empty():
+            # If list has only one node_object then assign None in start.
+            if self.start.next is None:
+                self.start = None
+            # If list has at least two node_objects then we will do this.
+            else:
+                # Get the reference of first variable.
+                temp = self.start
+                while temp.next is not None:
+                    temp = temp.next
+                temp.prev.next = None
+
+        # If list is empty then do nothing.
+        else:
+            pass
+
+    # Ye abhi create karna hai.
+    def delete_item(self):
+        pass
 
 
 # Que:3-> Define a method is_empty() to check if the linked list
@@ -78,6 +147,17 @@ class DLL:
 # element at the starting of the list.
 # Que:5-> In class DLL define a method insert_at_last() to
 # insert an element at  the end of the list.
+# Que:6-> In class DLL define a method search() to find the node with specified
+# element value.
+# Que:7-> In class DLL define a method insert_after() to insert a new node after
+# a given node list.
+# Que:8-> In class DLL define a method to print all elements of the list.
+# Que:9-> In class DLL define a method to access all elements of the list in
+# a sequence.
+# Que:10-> In class DLL define a method  delete_first() to delete first element.
+# Que:11-> In class DLL define a method delete_last() to delete last element.
+# Que:12-> In class DLL define a method delete_item() to delete specified
+# element from the list.
 
 
 # Testing code
@@ -86,9 +166,21 @@ myList = DLL()
 myList.insert_at_start(51)
 # myList.insert_at_start(101)
 # myList.insert_at_last(500)
-myList.insert_at_last(400)
+# myList.insert_at_last(400)
 # myList.insert_at_last(1000)
 
 myList.printObject()
+# a = int(input("\n enter the data that you want to search: "))
+# b = int(input(" enter the data that you want to insert: "))
+# myList.delete_first()
+# myList.delete_first()
+# myList.delete_first()
+# myList.delete_first()
+myList.delete_first()
+
+# myList.insert_after(a, b)
+print()
+myList.printObject()
+# print(myList.search(a))
 # for i in myList:
 #     print(i)
